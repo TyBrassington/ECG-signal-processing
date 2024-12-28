8c6a3b50ef6a42e44db8cad28c8fa811d546d5ee
TyBrassington <98743450+TyBrassington@users.noreply.github.com>
2024-12-28 02:23:31 -0800

    feat(filter): add movingWindow() and panTompkinsFiler()

--- src/PanTompkins.java
+++ src/PanTompkins.java
@@ -49,5 +49,30 @@ public class PanTompkins {
         return output;
     }
 
+    public static double[] movingWindow(double[] signal, int windowSize){
+        int n = signal.length;
+        double[] output = new double[n];
+        double sum = 0;
 
+        //init moving window
+        for (int i = 0; i < windowSize && i < n; i++) {
+            sum += signal[i];
+        }
+        //slide window
+        for (int i = 0; i < n; i++) {
+            if (i > windowSize) sum -= signal[i - windowSize]; //subtract oldest sample
+            sum += signal[i]; //add newest sample
+            output[i] = sum / windowSize;
+        }
+        return output;
+    }
+
+    public static double[] panTompkinsFilter(double[] signal, int windowSize){
+        //apply Pan-Tompkins
+        double[] bandSignal = bandPassFilter(signal);
+        double[] diffSignal = differentiation(bandSignal);
+        double[] squaredSignal = square(diffSignal);
+
+        return movingWindow(squaredSignal, windowSize);
+    }
 }
