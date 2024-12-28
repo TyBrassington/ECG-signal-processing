06c3a703e740de890a967088d796b853125378db
TyBrassington <98743450+TyBrassington@users.noreply.github.com>
2024-12-28 02:36:37 -0800

    test(main): placeholder test code in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        //placeholder
+        double[] signal = {};
 
+        int windowSize = 5;
+
+        double result[] = PanTompkins.panTompkinsFilter(signal, windowSize);
+
+        System.out.println("Filtered Signal:");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
     }
 }
